f3aec2001805532956ce0e4f2e1438fbd9aad98e
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-04-14 20:53:32 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_posts.rb
+++ db/migrate/001_create_posts.rb
@@ -1,10 +1,10 @@
-class CreatePosts < ActiveRecord::Migration 
+class CreatePosts < ActiveRecord::Migration
   def change
-    create_table :posts do |t| 
+    create_table :posts do |t|
       t.string :title
-      t.text :description 
+      t.text :description
 
-      t.timestamps null: false 
-    end 
-  end 
+      t.timestamps null: false
+    end
+  end
 end 
